910064b67b6826e2d0d322efb46e4b2be5903cee
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-14 15:49:23 +0100

    Add duck rotation

--- src/geometry/point.rs
+++ src/geometry/point.rs
@@ -8,4 +8,15 @@ impl Point2D {
     pub const fn new(x: f64, y: f64) -> Self {
         Self { x, y }
     }
+    pub fn rotate_around(&self, relative_to: Self, rotate_by: f64) -> Self {
+        let sine = rotate_by.sin();
+        let cosine = rotate_by.cos();
+        let transposed = Self::new(self.x - relative_to.x, self.y - relative_to.y);
+        let rotated = Self::new(
+            cosine * transposed.x - sine * transposed.y,
+            sine * transposed.x + cosine * transposed.y,
+        );
+        let retransposed = Self::new(rotated.x + relative_to.x, rotated.y + relative_to.y);
+        retransposed
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{thread::sleep, time::Duration};
+use std::{
+    ops::Mul,
+    thread::sleep,
+    time::{self, Duration},
+};
 
 use duck::{BASE_TRIANGLES, BEAK_TRIANGLE, EYE_TRIANGLE};
 use geometry::Point2D;
@@ -13,23 +17,33 @@ enum ColorPair {
     Beak = 2,
 }
 
+const MIDDLE: Point2D = Point2D::new(0.5, 0.5);
+const ROTATION_SPEED: f64 = 2.0;
+
 fn main() {
     let window = initscr();
     pancurses::start_color();
     pancurses::curs_set(0);
     pancurses::init_pair(ColorPair::Body as i16, COLOR_YELLOW, COLOR_BLACK);
     pancurses::init_pair(ColorPair::Beak as i16, COLOR_RED, COLOR_BLACK);
+    let time_reference = time::SystemTime::now();
     loop {
         let (max_y, max_x) = window.get_max_yx();
+        let rotate_by = time_reference
+            .elapsed()
+            .unwrap_or_default()
+            .as_secs_f64()
+            .mul(-ROTATION_SPEED);
         for y in (0..max_y).rev() {
             for x in 0..max_x {
                 let current_point =
                     Point2D::new((x as f64) / (max_x as f64), (y as f64) / (max_y as f64));
+                let current_point_rotated = current_point.rotate_around(MIDDLE, rotate_by);
                 let inside_base = BASE_TRIANGLES
                     .into_iter()
-                    .any(|triangle| triangle.is_point_inside(&current_point));
-                let inside_eye = EYE_TRIANGLE.is_point_inside(&current_point);
-                let inside_beak = BEAK_TRIANGLE.is_point_inside(&current_point);
+                    .any(|triangle| triangle.is_point_inside(&current_point_rotated));
+                let inside_eye = EYE_TRIANGLE.is_point_inside(&current_point_rotated);
+                let inside_beak = BEAK_TRIANGLE.is_point_inside(&current_point_rotated);
                 if inside_beak {
                     window.color_set(ColorPair::Beak as i16);
                     window.mvaddch(y, x, '#');
