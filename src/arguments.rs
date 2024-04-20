c4fc4adf74add902bae346eafb93d2f49249103a
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-04-20 14:46:45 +0200

    Add descriptions to arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -4,7 +4,9 @@ use clap::{command, Parser};
 #[command(version, about)]
 pub struct Arguments {
     #[arg(short, long, default_value_t = -2.0)]
+    /// Duck rotation speed [rad/s] (counter-clockwise)
     pub rotation_speed: f64,
     #[arg(short, long)]
+    /// Post-processes that should be applied (antialias, outline)
     pub postprocesses: Vec<String>,
 }
